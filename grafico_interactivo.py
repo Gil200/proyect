52342ab9977c4d5630b2fdd1f0668143b08dc1f6
Gil200 <pgilantunano@gmail.com>
2025-04-20 17:34:50 +0200

    grafico_interactivo.py

--- grafico_interactivo.py
+++ grafico_interactivo.py
@@ -1,17 +1,30 @@
+import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
+
+# Título de la app
+st.title("Gráfico interactivo - Business Case")
+
+# Cargar los datos
 data = pd.read_excel("Business Case Data.xlsx")
+
+# Filtrar por canal
 superm = data[data['CHANNEL'] == 'Supermarket']
 conv = data[data['CHANNEL'] == 'Convenience']
-chan_s_b = superm['BRAND1_BIG_post']-superm['BRAND1_BIG_pre']
-chan_s_m = superm['BRAND1_MEDIUM_post']-superm['BRAND1_MEDIUM_pre']
-chan_c_b = conv['BRAND1_BIG_post']-conv['BRAND1_BIG_pre']
-chan_c_m = conv['BRAND1_MEDIUM_post']-conv['BRAND1_MEDIUM_pre']
+
+# Calcular diferencias
+chan_s_b = superm['BRAND1_BIG_post'] - superm['BRAND1_BIG_pre']
+chan_s_m = superm['BRAND1_MEDIUM_post'] - superm['BRAND1_MEDIUM_pre']
+chan_c_b = conv['BRAND1_BIG_post'] - conv['BRAND1_BIG_pre']
+chan_c_m = conv['BRAND1_MEDIUM_post'] - conv['BRAND1_MEDIUM_pre']
+
+# Promedios
 avg_sup_b = chan_s_b.mean()
 avg_sup_m = chan_s_m.mean()
 avg_conv_b = chan_c_b.mean()
 avg_conv_m = chan_c_m.mean()
-import plotly.express as px
+
+# Preparar datos para graficar
 mean_values = [avg_sup_b, avg_sup_m, avg_conv_b, avg_conv_m]
 labels = ['Big-Supermarket', 'Medium-Supermarket', 'Big-Convenience', 'Medium-Convenience']
 colors = ['blue', 'green', 'yellow', 'purple']
@@ -22,14 +35,18 @@ df_plot = pd.DataFrame({
     'Color': colors
 })
 
-fig = px.bar(df_plot,
-             x='Segment',
-             y='Change',
-             color='Segment',
-             color_discrete_sequence=colors,
-             title='Average change in Supermarkets and Convenience Stores',
-             labels={'Change': 'Avg Change (Post - Pre)'}
-            )
+# Crear gráfico interactivo
+fig = px.bar(
+    df_plot,
+    x='Segment',
+    y='Change',
+    color='Segment',
+    color_discrete_sequence=colors,
+    title='Average change in Supermarkets and Convenience Stores',
+    labels={'Change': 'Avg Change (Post - Pre)'}
+)
 
 fig.update_layout(showlegend=False)
-fig.show()
+
+# Mostrar en Streamlit
+st.plotly_chart(fig)
